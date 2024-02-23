d0adc25b3bc447771f80dd0b5d769be23e86839a
nyxrotten <nes_calina@hotmail.com>
2024-02-23 21:22:12 +0100

    21:22

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-qq   
+const botones = document.querySelectorAll(".boton-jugada");
+const resultados = document.getElementById("resultados");
+const contadorUsuario = document.getElementById("contador-usuario");
+const contadorOrdenador = document.getElementById("contador-ordenador");
 
 
 
-g  fff
+console.log(resultados)
+
+
+
+
+
