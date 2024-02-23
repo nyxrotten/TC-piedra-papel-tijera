6909447f38f5afb1927d4bec461283d3619acc65
nyxrotten <nes_calina@hotmail.com>
2024-02-23 21:52:29 +0100

    21:52

--- js/script.js
+++ js/script.js
@@ -3,9 +3,36 @@ const resultados = document.getElementById("resultados");
 const contadorUsuario = document.getElementById("contador-usuario");
 const contadorOrdenador = document.getElementById("contador-ordenador");
 
+let jugadaUsuario;
+let jugadaOrdenador;
 
 
-console.log(resultados)
+//botones.addEventListener('click', function() {})
+
+
+function generarJugada(jugadaUsuario, jugadaOrdenador) {
+    if (
+        (jugadaUsuario === "piedra" && jugadaOrdenador === "tijera") ||
+        (jugadaUsuario === "tijera" && jugadaOrdenador === "papel") ||
+        (jugadaUsuario === "papel" && jugadaOrdenador === "piedra")
+        
+    )
+    {
+        contador++;
+        console.log("Ganaste")
+    }
+    else if (jugadaUsuario === jugadaOrdenador) {
+        console.log("Empate")
+    }
+    else  {
+        console.log("Perdiste")
+    }
+
+}
+
+generarJugada("piedra", "papel")
+
+
 
 
 
